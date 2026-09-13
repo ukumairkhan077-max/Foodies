f550bded271366948317e1bfcd5b44edbf03dc53
Umair khan <ukumairkhan077@gmail.com>
2026-09-13 03:39:55 -0700

    changes the css for many files

--- food-restaurant/src/data/chefsData.js
+++ food-restaurant/src/data/chefsData.js
@@ -3,19 +3,19 @@ const chefsData = [
     id: 1,
     name: "Marco Rossi",
     role: "Head Chef",
-    image: "/images/chefs/chef1.jpg",
+    image: "https://media.istockphoto.com/id/157589277/photo/portrait-of-happy-smiling-cook-in-chefs-hat-and-uniform.jpg?s=612x612&w=0&k=20&c=WJ-xEqqnRgUqtQ6ivTygd1DfPMn1oT41s0Czps5qHSU=",
   },
   {
     id: 2,
     name: "Elena Novak",
     role: "Pastry Chef",
-    image: "/images/chefs/chef2.jpg",
+    image: "https://media.istockphoto.com/id/480379746/photo/cook-decorating-a-plate-at-a-restaurant.jpg?s=612x612&w=0&k=20&c=ZfRzr7xqK9a89DzUZ1h-oEHUEzi6FF0hiDpljIdtgt0=",
   },
   {
     id: 3,
     name: "Sana Malik",
     role: "Sous Chef",
-    image: "/images/chefs/chef3.jpg",
+    image: "https://media.istockphoto.com/id/137049093/photo/female-chef.jpg?s=612x612&w=0&k=20&c=cn8cwiWC2nncWN8OK9f4kignvbMbETPpHJU1uujNg8c=",
   },
 ];
 
