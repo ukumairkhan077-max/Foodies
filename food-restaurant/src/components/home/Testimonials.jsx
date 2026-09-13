096aaaaf55f0f7f97deeb6d252cf392cedb56b10
Umair khan <ukumairkhan077@gmail.com>
2026-09-13 03:38:51 -0700

    make correct the use of images in the section

--- food-restaurant/src/components/home/Testimonials.jsx
+++ food-restaurant/src/components/home/Testimonials.jsx
@@ -3,17 +3,17 @@ import SectionTitle from "../common/SectionTitle";
 const testimonials = [
   {
     name: "Ali R.",
-    image: "/images/restaurant/customer-1.jpg",
+    image: "https://media.istockphoto.com/id/157589277/photo/portrait-of-happy-smiling-cook-in-chefs-hat-and-uniform.jpg?s=612x612&w=0&k=20&c=WJ-xEqqnRgUqtQ6ivTygd1DfPMn1oT41s0Czps5qHSU=",
     text: "The burgers were amazing and delivery was incredibly fast.",
   },
   {
     name: "Sara K.",
-    image: "/images/restaurant/customer-2.jpg",
+    image: "https://media.istockphoto.com/id/480379746/photo/cook-decorating-a-plate-at-a-restaurant.jpg?s=612x612&w=0&k=20&c=ZfRzr7xqK9a89DzUZ1h-oEHUEzi6FF0hiDpljIdtgt0=",
     text: "Best pizza in town! Fresh ingredients and great taste.",
   },
   {
     name: "Usman T.",
-    image: "/images/restaurant/customer-3.jpg",
+    image: "https://media.istockphoto.com/id/137049093/photo/female-chef.jpg?s=612x612&w=0&k=20&c=cn8cwiWC2nncWN8OK9f4kignvbMbETPpHJU1uujNg8c=",
     text: "Excellent service and delicious food. Highly recommended!",
   },
 ];
